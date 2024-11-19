88951395c933cd46a29e6ada27d919603c5dcb55
nlucaccioni <n.lucaccioni@gmail.com>
2024-11-18 20:59:59 -0600

    Add initial grid logic for 16x16 and add header.

---
+++ script.js
@@ -0,0 +1,19 @@
+let grid = document.querySelector("main");
+let gridSize = 16;
+
+function drawGrid() {
+    for (let i = 1; i <= gridSize; i++){
+        const gridRow = document.createElement('div');
+        gridRow.classList.add("gridRow");
+
+        for (let z = 1; z <= gridSize; z++){
+            const gridItem = document.createElement('div');
+            gridItem.classList.add("gridItem");
+            gridRow.appendChild(gridItem);
+        }
+
+        grid.appendChild(gridRow);
+    }
+}
+
+drawGrid();
