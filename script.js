cc922034967247b1104fa4a6403a8c30e89321c8
nlucaccioni <n.lucaccioni@gmail.com>
2024-11-18 23:02:47 -0600

    Add button and reset functionality. Finish styling.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let grid = document.querySelector("#gridContain");
 let gridSize = 16;
 
-function drawGrid() {
+function drawGrid(resolution) {
     for (let i = 1; i <= gridSize; i++){
+        resolution = gridSize;
         const gridRow = document.createElement('div');
         gridRow.classList.add("gridRow");
 
@@ -13,16 +14,34 @@ function drawGrid() {
         }
 
         grid.appendChild(gridRow);
+
+        const gridItems = document.querySelectorAll(".gridItem");
+
+        gridItems.forEach(gridItem => {
+            gridItem.addEventListener('mouseover', () => {
+                gridItem.style.backgroundColor = "var(--OFF_WHITE)";
+            })
+        })
     }
 }
 
 drawGrid();
 
 
-const gridItems = document.querySelectorAll(".gridItem");
+const resetBtn = document.querySelector("#reset");
 
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.style.backgroundColor = "var(--OFF_WHITE)";
+resetBtn.addEventListener('click', () => {
+    gridSize = prompt('Choose your new sketching resolution.', '16');
+
+    while (gridSize > 100 || gridSize < 1)  {
+        alert('Please choose a resolution beween 1-100.');
+        gridSize = prompt('Choose your new sketching resolution.', '0');
+    }
+
+    const gridRow = document.querySelectorAll(".gridRow");
+    gridRow.forEach(row => {
+        row.remove();
     })
+
+    drawGrid(gridSize);
 })
